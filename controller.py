ade518c928ec863539ff3c37c97c9dd7d639a72b
J-Catherine <865687826@qq.com>
2019-04-20 20:19:51 +0800

    Use docopt in controller.py to improve the CLI. Try to use gpustat to enhance the function of reporter.

--- controller.py
+++ controller.py
@@ -1,4 +1,16 @@
-import os, sys
+"""
+Usage:
+    controller.py <file>  [-k|--kill] [-s|--start]
+
+Arguments:
+    file                    The csv file with information of servers
+
+Options:
+    -s --start              Start reporters on servers
+    -k --kill               Kill all reporters on servers
+"""
+import docopt
+import os
 
 
 def get_ip():
@@ -10,128 +22,42 @@ def get_ip():
     return 0, "0.0.0.0"
 
 
-def update_with_screen(lines=None):
-    if lines is None:
-        with open("servers.csv", "r")as fin:
-            lines = fin.readlines()
+def update_with_screen(work_servers, kill=False):
+    with open(work_servers, "r")as fin:
+        lines = fin.readlines()
     for each in lines[1:]:
-        data = each.split(",")
+        data = each.strip().split(",")
         with open("run.sh", "w") as fout:
-            fout.write(
-                f"""#!/usr/bin/expect
-                
-                set host {data[0]}
-                set user {data[1]}
-                set timeout 2
-                
-                spawn ssh $user@$host
-                expect "*password:*"
-                send "{data[2]}\r"
+            cmd = f"""#!/usr/bin/expect
+            set host {data[0]}
+            set user {data[1]}
+            set timeout 2
+            
+            spawn ssh $user@$host
+            expect "*password:*"
+            send "{data[2]}\\r"
+            expect "*]#"
+            send "screen -S nvidia_reporter -X quit\\r"
+            expect "*]#"
+            send "rm -rf .nvidia_reporter{data[0][-3:]}/\\r"\n"""
+            if not kill:
+                cmd += f"""expect "*]#"
+                send "mkdir .nvidia_reporter\\r"
                 expect "*]#"
-                send "screen -S nvidia_reporter -X quit\r"
+                send "cd .nvidia_reporter\\r"
                 expect "*]#"
-                send "rm -rf .nvidia_reporter{data[0][-3:]}/\r"
+                send "wget http://{base_url}/reporter\\r"
                 expect "*]#"
-                send "mkdir .nvidia_reporter\r"
+                send "mv reporter reporter.py\\r"
                 expect "*]#"
-                send "cd .nvidia_reporter\r"
+                send "screen -S nvidia_reporter\\r"
                 expect "*]#"
-                send "wget http://{base_url}/reporter\r"
+                send "python reporter.py {base_url}\\r"
                 expect "*]#"
-                send "mv reporter reporter.py\r"
-                expect "*]#"
-                send "screen -S nvidia_reporter\r"
-                expect "*]#"
-                send "python reporter.py {base_url}\r"
-                expect "*]#"
-                send "exit\r"
-                expect eof""")
+                send "exit\\r"
+                expect eof"""
+            fout.write(cmd)
         os.system("chmod 777 run.sh && ./run.sh")
-        print(f"\n====={data[0]}, ok!=====\n")
-    os.system("rm run.sh")
-
-
-def stop_screen():
-    with open("servers.csv", "r")as fin:
-        lines = fin.readlines()
-    for each in lines[1:]:
-        data = each.strip('\n').split(",")
-        with open("run.sh", "w") as fout:
-            fout.write(
-                f"""#!/usr/bin/expect
-                
-                set host {data[0]}
-                set user {data[1]}
-                set timeout 2
-                
-                spawn ssh $user@$host
-                expect "*password:*"
-                send "{data[2]}\r"
-                expect "*]#"
-                send "screen -S nvidia_reporter -X quit\r"
-                expect "*]#"
-                send "rm -rf .nvidia_reporter{data[0][-3:]}/\r"
-                expect "*]#"
-                send "exit\r"
-                expect eof""")
-        os.system("chmod 777 run.sh && ./run.sh")
-        print(f"\n====={data[0]}, ok!=====\n")
-    os.system("rm run.sh")
-
-
-def create_users():
-    shell_context = """#!/bin/sh
-
-echo \\\"Remove old account if exist\\\"
-userdel -r -f \\\"\\$1\\\" || true
-echo \\\"Add new account for \\$1\\\"
-useradd -m \\$1 -d /remote-home/\\$1 -u \\$2 -U
-echo \\\"Change password for \\$1\\\"
-echo \\\"\\$1\\n\\$1\\\" | passwd \\\"\\$1\\\"
-mkdir /remote-home/\\$1
-chown \\$1:\\$1 /remote-home/\\$1
-echo \\\"Add \\$1 to group sudo\\\"
-chsh \\$1 -s /bin/bash
-
-echo \\\"DONE\\\" """
-    with open("users.csv", "r")as fin:
-        lines = fin.readlines()
-    commands = ""
-    for line in lines[1:]:
-        if line.startswith("#"):
-            continue
-        line = line.strip('\n').split(',')
-        commands += f"""send "sudo bash ./add_remote.sh {line[0]} {line[1]}\n" \nexpect "*]#"\n"""
-    with open("servers.csv", "r")as fin:
-        lines = fin.readlines()
-    for each in lines[1:]:
-        data = each.strip('\n').split(",")
-        with open("run.sh", "w") as fout:
-            fout.write(
-                f"""#!/usr/bin/expect
-                
-                set host {data[0]}
-                set user {data[1]}
-                set timeout 2
-                
-                spawn ssh $user@$host
-                expect "*password:*"
-                send "{data[2]}\r"
-                expect "*]#"
-                send "sudo su\r"
-                expect "*password:*"
-                send "{data[2]}\r"
-                expect "*]#"
-                send "echo -e '"""
-                + shell_context +
-                """ '> add_remote.sh\r"
-                expect "*]#"\n"""
-                + commands +
-                """send "rm add_remote.sh\r"
-                expect "*]#"
-                send "exit\r"
-                expect eof""")
-        os.system("chmod 700 run.sh && ./run.sh")
         print(f"\n====={data[0]}, ok!=====\n")
     os.system("rm run.sh")
 
@@ -142,13 +68,10 @@ if __name__ == "__main__":
         print("Can't get the IP address.")
     else:
         base_url = ip + ':8997'
-        if len(sys.argv) == 1:
-            print("-u Update all reporters (with servers.csv)")
-            print("-s Stop all reporters (with servers.csv)")
-            print("-c Create remote-home for users (with users.csv)")
-        elif sys.argv[1] == '-u':
-            update_with_screen()
-        elif sys.argv[1] == '-s':
-            stop_screen()
-        elif sys.argv[1] == '-c':
-            create_users()
+        args = docopt.docopt(__doc__, version='GPU Info Controller v1.1')
+        if args['--start']:
+            update_with_screen(args['<file>'])
+        elif args['--kill']:
+            update_with_screen(args['<file>'], True)
+        else:
+            print(__doc__)
--- reporter.py
+++ reporter.py
@@ -1,6 +1,6 @@
-import os
-import re
+import datetime
 import json
+import subprocess
 import sys
 import time
 
@@ -13,83 +13,12 @@ except ImportError:
     
     version = 3
 
-err_codes = {
-    1001: "Can't get ip",
-    1002: "Can't find gpus",
-    1003: "Can't gpu info",
-}
-
-
-def get_ip():
-    result = os.popen("ifconfig | grep addr:10")
-    ips = result.read().strip().split()
-    for each in ips:
-        if each[:5] == "addr:":
-            return 0, each[5:]
-    return 0, "0.0.0.0"
-
-
-def get_devices():
-    result = os.popen("nvidia-smi -L")
-    lines = result.readlines()
-    devices = []
-    for i, line in enumerate(lines):
-        if line[:3] == 'GPU':
-            uuid = re.findall(r"\(UUID: [\w-]+\)", line)[0][7:-1]
-            name = re.findall(r"GPU \d:[\w ]+\(", line)[0][7:-2]
-            devices.append({
-                "id": i,
-                "name": name,
-                "uuid": uuid
-            })
-        else:
-            return 1002, []
-    return 0, devices
-
-
-def get_line(line):
-    line = re.findall(r"<[\w]+>[\w% ]+</[\w]+>", line)
-    if line:
-        line = line[0]
-        name = line.split('>')[0][1:]
-        value = line.split('>')[1].split('<')[0]
-        return name, value
-    else:
-        return "", ""
-
-
-def get_info_by_id(_id):
-    result = os.popen("nvidia-smi --_id=" + str(_id) + " -q --xml-format")
-    lines = result.readlines()
-    need = {"gpu_util", "memory_util", "encoder_util", "decoder_util", "gpu_temp",
-            "gpu_temp_max_threshold", "gpu_temp_slow_threshold", "fan_speed"}
-    info = {}
-    for line in lines:
-        name, value = get_line(line)
-        if name in need:
-            info[name] = value.split()[0]
-        if name == 'pid':
-            pid = value
-            grep = os.popen("grep \"Uid\" /proc/" + pid + "/status").readlines()
-            uid = grep[0].split("\t")[1]
-            username = os.popen("getent passwd " + uid).readlines()[0].split(":")[0]
-            info["username"] = username
-    return info
-
 
 def get_info():
-    err, ip = get_ip()
-    if err:
-        return err, {"err_code": err}
-    err, devices = get_devices()
-    if err:
-        return err, {"err_code": err}
-    for i, _ in enumerate(devices):
-        info = get_info_by_id(i)
-        for name in info:
-            devices[i][name] = info[name]
-            devices[i]["ip"] = ip
-    return 0, devices
+    lines = subprocess.Popen("gpustat", shell=True, stdout=subprocess.PIPE,
+                             stderr=subprocess.DEVNULL).stdout.readlines()
+    lines = [line.decode('utf-8') for line in lines]
+    return 0, lines
 
 
 def send_info():
@@ -98,18 +27,12 @@ def send_info():
     if version == 3:
         data = bytes(data, 'utf8')
     try:
-        if err:
-            headers = {'Content-Type': 'application/json'}
-            request = urllib.Request(url=err_report_url, headers=headers, data=data)
-            _ = urllib.urlopen(request)
-            print("send err info.", err)
-        else:
-            headers = {'Content-Type': 'application/json'}
-            request = urllib.Request(url=report_url, headers=headers, data=data)
-            _ = urllib.urlopen(request)
-            print("send info.")
+        headers = {'Content-Type': 'application/json'}
+        request = urllib.Request(url=report_url, headers=headers, data=data)
+        _ = urllib.urlopen(request)
+        print("send info |", datetime.datetime.now())
     except urllib.URLError:
-        print("fail to send")
+        print("fail to send |", datetime.datetime.now())
 
 
 def keep_sending():
@@ -120,8 +43,14 @@ def keep_sending():
 
 
 if __name__ == "__main__":
-    base_url = sys.argv[1]
-    report_url = "http://" + base_url + "/post"
-    err_report_url = "http://" + base_url + "/err"
-    time_interval = 180
-    keep_sending()
+    if len(sys.argv) < 2 or sys.argv[1] == 'test':
+        print(get_info()[1])
+    else:
+        base_url = sys.argv[1]
+        report_url = "http://" + base_url + "/post"
+        try:
+            time_interval = int(sys.argv[2])
+        except (IndexError, ValueError):
+            #  TODO: 做笔记：多异常的捕获，异常的层级关系
+            time_interval = 180
+        keep_sending()
